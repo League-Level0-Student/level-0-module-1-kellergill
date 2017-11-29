c10864e5f3c5b6b0f760ee994c50bc45983e83a3
gill.keller <gill.keller@SEMS-259-17.SEMS259.LAB>
2017-11-29 14:55:29 -0800

    Today we worked on the riddler, XGeeks.

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,14 +10,24 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+		int score=0;
 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String answer=JOptionPane.showInputDialog(null, "Forward I am heavy, backward I am not. What am I?");
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if(answer.equals("Forward I am ton, backward I am not.")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score+=1;
+		}
 
 		// 5. Otherwise, say "wrong" and tell them the answer
+		else{
+			JOptionPane.showMessageDialog(null, "Dude, that's the wrong answer you succ.");
+		}
 
 		// 6. Add some more riddles
+		String answer1=JOptionPane.showInputDialog(null, "What's black, white, and red all over?");
 
 		// 2. Make a pop up to show the score.
 		
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -15,8 +15,32 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
+	    String Keller=" Telekinesis";
+	    String JacobP=" Making people salty";
+	    String Derek=" The power of god and anime";
+	    String JacobJ=" Teleportation";
+	    String SanchithaT=" Mind reader";
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+	    String name=JOptionPane.showInputDialog("Enter a name");
+	       if(name.equals("Keller")) {
+	           JOptionPane.showMessageDialog(null," Your power is"+Keller );
+	    }
+	       if(name.equals("JacobP")) {
+	           JOptionPane.showMessageDialog(null," Your power is"+JacobP );
+	    }
+	       if(name.equals("Derek")) {
+	    	   JOptionPane.showMessageDialog(null," Your power is"+Derek );
+	    }
+	       if(name.equals("JacobJ")) {
+	    	   JOptionPane.showMessageDialog(null," Your power is"+JacobJ );
+	    }
+	       if(name.equals("SanchithaT")) {
+	    	   JOptionPane.showMessageDialog(null," Your power is"+SanchithaT );
+	    }
+	       
+	    	   
+	      
 
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
 
